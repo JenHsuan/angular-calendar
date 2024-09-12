14ec140b267089d8ee17490d2420ba9b1e8fae18
Sean Hsieh <hsiehs@fortinet.com>
2024-09-12 09:52:09 +0800

    Update

--- src/app/calendar/calendar-widget/calendar-widget-overlay/calendar-widget-overlay.component.ts
+++ src/app/calendar/calendar-widget/calendar-widget-overlay/calendar-widget-overlay.component.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectorRef, Component, ElementRef, inject, ViewChild } from '@angular/core';
-import { CALENDAR_DATES_ITEM_STYLE_MAP, CalendarWidgetItemType } from '../service/calendar-widget.domain';
+import { FormsModule } from '@angular/forms';
+
+import _ from 'lodash';
+
+import { CALENDAR_DATES_ITEM_STYLE_MAP, CalendarWidgetItemType, YEAR_CALENDAR_ITEM_STYLE_MAP } from '../service/calendar-widget.domain';
 
 const MONTHS = [
   "January",
@@ -22,6 +26,7 @@ const MONTHS = [
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule
   ],
   templateUrl: './calendar-widget-overlay.component.html',
   styleUrl: './calendar-widget-overlay.component.scss'
@@ -30,8 +35,14 @@ export class CalendarWidgetOverlayComponent {
   month: number;
   year: number;
   date: Date;
+  hour: string;
+  minute: string;
+
+  isCalendarDatesItemsDisplayed = true;
+  yearCalendarSize = 25;
 
   @ViewChild('calendarDates') calendarDates: ElementRef;
+  @ViewChild('yearCalendaritems') yearCalendaritems: ElementRef;
 
   private cd = inject(ChangeDetectorRef);
 
@@ -39,10 +50,20 @@ export class CalendarWidgetOverlayComponent {
     return `${MONTHS[this.month]} ${this.year}`;
   }
 
+  get candidateHours(): string[] {
+    return Array(24).fill(0).map((data, index) => {
+      return index < 10 ? `0${index}` : `${index}`;
+    });
+  }
+
+  get candidateMinutes(): string[] {
+    return Array(60).fill(0).map((data, index) => {
+      return index < 10 ? `0${index}` : `${index}`;
+    });
+  }
+
   ngAfterViewInit() {
-    this.date = new Date();
-    this.year = this.date.getFullYear();
-    this.month = this.date.getMonth();
+    this.resetDateTimeProperties();
 
     this.update();
   }
@@ -52,62 +73,101 @@ export class CalendarWidgetOverlayComponent {
   }
 
   update() {
-    let dayone = new Date(this.year, this.month, 1).getDay();
-    let lastdate = new Date(this.year, this.month + 1, 0).getDate();
-    let monthlastdate = new Date(this.year, this.month, 0).getDate();
-    let dayend = new Date(this.year, this.month, lastdate).getDay();
+    if (this.isCalendarDatesItemsDisplayed) {
+      const dayone = new Date(this.year, this.month, 1).getDay();
+      const lastdate = new Date(this.year, this.month + 1, 0).getDate();
+      const monthlastdate = new Date(this.year, this.month, 0).getDate();
+      const dayend = new Date(this.year, this.month, lastdate).getDay();
+  
+      //remove existed items
+      while (this.calendarDates.nativeElement.firstChild) {
+        this.calendarDates.nativeElement.removeChild(this.calendarDates.nativeElement.firstChild);
+      }
+  
+      //inactive
+      for (let i = dayone; i > 0; i--) {
+        this.calendarDates.nativeElement.appendChild(this.createCalendarDatesItem((monthlastdate - i + 1).toString(), CalendarWidgetItemType.INACTIVE, new Date(this.year, this.month - 1, monthlastdate - i + 1)));
+      }
+  
+      //active
+      for (let i = 0; i < lastdate; i++) {
+        const type = i === (this.date.getDate() - 1) && this.month === this.date.getMonth() && this.year === this.date.getFullYear() ? CalendarWidgetItemType.ACTIVE : CalendarWidgetItemType.NONE;
+        this.calendarDates.nativeElement.appendChild(this.createCalendarDatesItem((i + 1).toString(), type, new Date(this.year, this.month, i + 1)));
+      }
+  
+      //inactive
+      for (let i = dayend; i < 6; i++) {
+        this.calendarDates.nativeElement.appendChild(this.createCalendarDatesItem((i - dayend + 1).toString(), CalendarWidgetItemType.INACTIVE, new Date(this.year, this.month + 1, i - dayend + 1)));
+      }
+    } else {
+      //remove existed items
+      while (this.yearCalendaritems.nativeElement.firstChild) {
+        this.yearCalendaritems.nativeElement.removeChild(this.yearCalendaritems.nativeElement.firstChild);
+      }
 
-    //remove existed items
-    while (this.calendarDates.nativeElement.firstChild) {
-      this.calendarDates.nativeElement.removeChild(this.calendarDates.nativeElement.firstChild);
-    }
+      const halfSize = Math.floor(this.yearCalendarSize / 2);
+      const startYear = this.year - halfSize;
 
-    //inactive
-    for (let i = dayone; i > 0; i--) {
-      this.calendarDates.nativeElement.appendChild(this.createCalendarDatesItem((monthlastdate - i + 1).toString(), CalendarWidgetItemType.INACTIVE, new Date(this.year, this.month - 1, monthlastdate - i + 1)));
-    }
+      for (let i = 0; i < halfSize; i++) {
+        this.yearCalendaritems.nativeElement.appendChild(this.createYearCalendarItem((startYear + i).toString(), CalendarWidgetItemType.INACTIVE));
+      }
 
-    //active
-    for (let i = 0; i < lastdate; i++) {
-      const type = i === (this.date.getDate() - 1) && this.month === this.date.getMonth() && this.year === this.date.getFullYear() ? CalendarWidgetItemType.ACTIVE : CalendarWidgetItemType.NONE;
-      this.calendarDates.nativeElement.appendChild(this.createCalendarDatesItem((i + 1).toString(), type, new Date(this.year, this.month, i + 1)));
-    }
+      this.yearCalendaritems.nativeElement.appendChild(this.createYearCalendarItem((startYear + halfSize).toString(), CalendarWidgetItemType.ACTIVE));
 
-    //inactive
-    for (let i = dayend; i < 6; i++) {
-      this.calendarDates.nativeElement.appendChild(this.createCalendarDatesItem((i - dayend + 1).toString(), CalendarWidgetItemType.INACTIVE, new Date(this.year, this.month + 1, i - dayend + 1)));
+      for (let i = halfSize + 1; i < this.yearCalendarSize; i++) {
+        this.yearCalendaritems.nativeElement.appendChild(this.createYearCalendarItem((startYear + i).toString(), CalendarWidgetItemType.INACTIVE));
+      }
     }
   }
 
   onNavigationButtonClicked(isPlus: boolean) {
-    this.month = isPlus ? this.month + 1 : this.month - 1;
-
-    if (this.month < 0 || this.month > 11) {
-      this.date = new Date(this.year, this.month, new Date().getDate());
-      this.year = this.date.getFullYear();
-      this.month = this.date.getMonth();
+    if (this.isCalendarDatesItemsDisplayed) {
+      this.month = isPlus ? this.month + 1 : this.month - 1;
+  
+      if (this.month < 0 || this.month > 11) {
+        this.date = new Date(this.year, this.month, new Date().getDate());
+        this.year = this.date.getFullYear();
+        this.month = this.date.getMonth();
+      } else {
+        this.date = new Date();
+      }
+  
+      this.update();
     } else {
-      this.date = new Date();
+      const delta = Math.floor(this.yearCalendarSize / 2);
+      this.year = isPlus ? this.year + delta : this.year - delta;
+      this.update();
     }
-
-    this.update();
   }
 
   onTodayButtonClicked() {
-    this.date = new Date();
-    this.year = this.date.getFullYear();
-    this.month = this.date.getMonth();
+    this.resetDateTimeProperties();
 
     this.update();
   }
 
-  private createCalendarDatesItem(displayedDate: string, type: CalendarWidgetItemType, date: Date): HTMLLIElement {
+  private resetDateTimeProperties() {
+    this.date = new Date();
+    this.year = this.date.getFullYear();
+    this.month = this.date.getMonth();
+    this.hour = this.date.getHours().toString();
+    this.minute = this.date.getMinutes().toString();
+
+    if (this.hour.length === 1) {
+      this.hour = `0${this.hour}`;
+    } 
+    
+    if (this.minute.length === 1) {
+      this.minute = `0${this.minute}`;
+    }
+  }
+
+  private createYearCalendarItem(displayedYear: string, type: CalendarWidgetItemType): HTMLLIElement {
     const item = document.createElement('li');
-    item.textContent = displayedDate;
-    item.setAttribute("style", CALENDAR_DATES_ITEM_STYLE_MAP.get(type) ?? "");
+    item.textContent = displayedYear;
+    item.setAttribute("style", YEAR_CALENDAR_ITEM_STYLE_MAP.get(type) ?? "");
     item.onclick = () => {
-      console.log(date);
-      this.onCalendarDatesItemClicked(date);
+      this.onYearCalendarItemClicked(displayedYear);
     };
 
     if (type !== CalendarWidgetItemType.ACTIVE) {
@@ -120,14 +180,66 @@ export class CalendarWidgetOverlayComponent {
       };
     }
 
-    this.calendarDates.nativeElement.appendChild(item);
+    return item;
+  }
+
+  private createCalendarDatesItem(displayedDate: string, type: CalendarWidgetItemType, date: Date): HTMLLIElement {
+    const item = document.createElement('li');
+    item.textContent = displayedDate;
+    item.setAttribute("style", CALENDAR_DATES_ITEM_STYLE_MAP.get(type) ?? "");
+    item.onclick = () => {
+      this.onCalendarDatesItemClicked(date);
+      console.log(date);
+    };
+
+    if (type !== CalendarWidgetItemType.ACTIVE) {
+      item.onmouseenter = () => {
+        item.style.backgroundColor = '#f2f2f2';
+      };
+  
+      item.onmouseleave = () => {
+        item.style.backgroundColor = '#fff';
+      };
+    }
 
     return item;
   }
 
+  onHourChanged(hour: string) {
+    this.hour = hour;
+    this.date.setHours(parseInt(this.hour));
+
+    console.log(this.date);
+  }
+
+  onMinuteChanged(minute: string) {
+    this.minute = minute;
+    this.date.setMinutes(parseInt(this.minute));
+
+    console.log(this.date);
+  }
+
+  onCurrentDateClicked() {
+    if (this.isCalendarDatesItemsDisplayed) {
+      this.isCalendarDatesItemsDisplayed = !this.isCalendarDatesItemsDisplayed;  
+    }
+
+    this.update();
+  }
+
   private onCalendarDatesItemClicked(date: Date) {
     this.date = date;
+    this.date.setHours(parseInt(this.hour));
+    this.date.setMinutes(parseInt(this.minute));
 
+    this.update();
+  }
+
+  private onYearCalendarItemClicked(year: string) {
+    this.year = parseInt(year);
+    this.date.setHours(this.year);
+    this.isCalendarDatesItemsDisplayed = true;
+    
     this.update();
   }
 }
--- src/app/calendar/calendar-widget/service/calendar-widget.domain.ts
+++ src/app/calendar/calendar-widget/service/calendar-widget.domain.ts
@@ -1,4 +1,3 @@
-
 export const CALENDAR_DATES_ITEM_STYLE = `
   font-size: 1.07rem; 
   cursor: pointer; 
@@ -21,6 +20,28 @@ export const ACTIVE_CALENDAR_DATES_ITEM_STYLE = `
   background-color: #007bff;
 `;
 
+export const YEAR_CALENDAR_DATES_ITEM_STYLE = `
+  font-size: 1.07rem; 
+  cursor: pointer; 
+  width: 2.8em;
+  height: 2em;
+  line-height: 2em; 
+  margin-top: 1.7em; 
+  margin-left: calc((100% / 5 - 2.8em) / 2);
+  margin-right: calc((100% / 5 - 2.8em) / 2);
+  border-radius: .2em;
+  text-align: center;
+`;
+
+export const INACTIVE_YEAR_CALENDAR_DATES_ITEM_STYLE = `
+  color: #aaa;
+`;
+
+export const ACTIVE_YEAR_CALENDAR_DATES_ITEM_STYLE = `
+  color: #fff;
+  background-color: #007bff;
+`;
+
 export enum CalendarWidgetItemType {
   INACTIVE,
   ACTIVE,
@@ -31,4 +52,10 @@ export const CALENDAR_DATES_ITEM_STYLE_MAP = new Map<CalendarWidgetItemType, str
   [ CalendarWidgetItemType.INACTIVE, `${CALENDAR_DATES_ITEM_STYLE}${INACTIVE_CALENDAR_DATES_ITEM_STYLE}`],
   [ CalendarWidgetItemType.ACTIVE, `${CALENDAR_DATES_ITEM_STYLE}${ACTIVE_CALENDAR_DATES_ITEM_STYLE}`],
   [ CalendarWidgetItemType.NONE, CALENDAR_DATES_ITEM_STYLE]
+]);
+
+export const YEAR_CALENDAR_ITEM_STYLE_MAP = new Map<CalendarWidgetItemType, string>([
+  [ CalendarWidgetItemType.INACTIVE, `${YEAR_CALENDAR_DATES_ITEM_STYLE}${INACTIVE_YEAR_CALENDAR_DATES_ITEM_STYLE}`],
+  [ CalendarWidgetItemType.ACTIVE, `${YEAR_CALENDAR_DATES_ITEM_STYLE}${ACTIVE_YEAR_CALENDAR_DATES_ITEM_STYLE}`],
+  [ CalendarWidgetItemType.NONE, YEAR_CALENDAR_DATES_ITEM_STYLE]
 ]);
